efcb3b0511eb0d5927fdc582a8a95410efe0bdab
sgioia <sgioia@cisco.com>
2020-11-13 11:56:40 +0100

    Initial commit to handle IWO

--- src/CWOM/Target.js
+++ src/CWOM/Target.js
@@ -3,8 +3,8 @@ module.exports = class Target {
     constructor(target) {
         if(!target) { target = {};}
         this.uuid = target.uuid || '';;
-        this.displayName = target.displayName || '';
-        this.className = target.className || '';;
+        this.displayName = target.displayName || '';  
+        this.className = target.className || '';
         this.costPrice = target.costPrice || '';
         this.aspects = new Aspects(target.aspects);
         this.environmentType = target.environmentType || '';
